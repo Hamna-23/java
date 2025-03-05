9628ac5f689e1715e50d7418bc4c1fd6791f22a0
Hamna-23 <hamnanizar234@gmail.com>
2025-03-05 11:51:39 +0530

    Add files via upload

---
+++ Strg.java
@@ -0,0 +1,12 @@
+public class Strg{
+public static void main(String[]args)
+{
+String message="hello";
+System.out.println(message.length());
+System.out.println(message.touppercase());
+System.out.println(message.tolowercase());
+String fname="computer";
+String sname="science";
+System.out.println(fname.concat(sname));
+}
+}
