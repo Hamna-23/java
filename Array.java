b7385f49e40c4aa6d5d34dc36eabec9505b357d4
Hamna-23 <hamnanizar234@gmail.com>
2025-03-05 11:53:17 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array{
+public static void main(String[]args)
+{
+String[] fruits={"apple","banana","grapes"};
+for(int i=0;i<fruits.length;i++){
+System.out.println(fruits[i]);
+}
+}
+}
---
+++ userstrg.java
@@ -0,0 +1,8 @@
+public class userstrg
+{
+public static void main(String[] args){
+String s="12345";
+int n=Integer.parseInt(s);
+System.out.println(n);
+}
+}
